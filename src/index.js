0b416ef89d97541b5938cee62eecc7f2cca446fa
qraztec <110706577+qraztec@users.noreply.github.com>
2023-08-09 11:36:03 -0700

    robotfriends project

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Card from './components/Card';
+import App from './containers/App';
+import CardList from './components/CardList';
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
+import {robots} from './Robots';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <div>
+      
+      {/* <CardList robots={robots}/> */}
+      <App />
+    </div>
+    
   </React.StrictMode>
 );
 
